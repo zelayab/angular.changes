9f46d555c58244f77dbb9afc8e632ac60a4b5428
Luciano S <thkapolandia@gmail.com>
2025-01-06 15:43:53 -0300

    feat(style): mejor estilo de sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,20 +7,17 @@ import { RouterModule, Router } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  @Input() versions: Array<{ version: string, ruta:string }> = [
+  @Input() versions: Array<{ version: string; ruta: string }> = [
     { version: 'Angular 18', ruta: 'angular-18' },
-    { version: 'Angular 19', ruta: 'angular-19' },
+    { version: 'Angular 19', ruta: 'angular-19' }
   ];
 
-
-  constructor(private router: Router) {
-
-  }
+  constructor(private router: Router) {}
 
   isActive(version: string): boolean {
-    return this.router.url === `/${version}`;
+    return this.router.url.includes(`/${version}`);
   }
 }
