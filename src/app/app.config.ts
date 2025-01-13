eebe27ec2e87899b9273da1f726a007b23cc3852
Ber Zelaya <73174174+zelayab@users.noreply.github.com>
2025-01-13 10:58:52 -0300

    feat(config): update router configuration to use hash location mode

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,21 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHighlightOptions } from 'ngx-highlightjs';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), // Configuración del enrutador
+    provideRouter(
+      routes, // Rutas de la aplicación
+      withHashLocation(), // Usa el modo de ubicación de hash
+    ),
     provideHttpClient(),   // Servicio HTTP
     provideHighlightOptions({
       fullLibraryLoader: () => import('highlight.js'),
       themePath: 'assets/highlight.js/styles/atom-one-dark.css', // Ruta al tema oscuro
     }),
     { provide: 'zone.js', useValue: null }, // Desactiva Zone.js
+
   ],
 };
